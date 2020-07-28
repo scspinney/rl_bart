fce86702b47d5c6e556afb557b07a1b5c955c022
SpinnSean <sean.c.spinney@gmail.com>
2020-07-28 16:48:39 -0400

    Added new feature to 68 experts to test out.

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ from maxent import *
 from likelihood import *
 import matplotlib.pyplot as plt
 
-maindir = '/data/neuroventure/behavioral/nback_and_bart/rl_bart'
+maindir = '/Users/sean/Projects/rl_bart/'
 year=1
 
 
 lr_decay=1
 
+N=68
 
 popPoints = [64,105,39,96,88,21,121,10,64,32,64,101,26,34,47,121,64,95,75,13,64,112,30,88,9,64,91,17,115,50]
 
@@ -18,14 +19,14 @@ popPoints = [64,105,39,96,88,21,121,10,64,32,64,101,26,34,47,121,64,95,75,13,64,
 
 # trajectories
 traj_paths = os.path.join(maindir,'data',f'V{year}')
-trajectories = [np.load(p,allow_pickle=True) for p in glob.glob(os.path.join(traj_paths,'**','traj.npy'))]
+trajectories = [np.load(p,allow_pickle=True) for p in np.sort(glob.glob(os.path.join(traj_paths,'**','traj.npy')))]
 
 #trajectories = np.load(traj_path,allow_pickle=True)
 #trajectories = trajectories[1:N]
 
 # feature matrices
 fmat_paths = os.path.join(maindir,'data',f'V{year}')
-feature_matrices = [np.load(p,allow_pickle=True) for p in glob.glob(os.path.join(fmat_paths,'**','fmat.npy'))]
+feature_matrices = [np.load(p,allow_pickle=True) for p in np.sort(glob.glob(os.path.join(fmat_paths,'**','fmat.npy')))]
 
 
 # transition prob: they are all the same so just pick one
@@ -33,6 +34,10 @@ Tprob = np.load(os.path.join(maindir,'data',f'transition_prob_Y{year}.npy'))
 
 #TODO: temp
 Tprob = Tprob[:-2,:,:-2]
+
+#TODO: temporary for debugging 12th feature
+trajectories = trajectories[:N]
+feature_matrices = feature_matrices[:N]
 
 
 weights = maxent_irl(maindir, year, feature_matrices, Tprob, gamma=1, trajectories=trajectories, lr=1E-4,lr_decay=lr_decay,n_iters=12,popPoints=popPoints, use_prior=False)
