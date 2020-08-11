22b6febd4fa2387831716cfa1738c1f821391f6c
SpinnSean <sean.c.spinney@gmail.com>
2020-08-11 15:19:41 -0400

    Added LL table function.

--- review.py
+++ review.py
@@ -6,6 +6,27 @@ import numpy as np
 
 np.set_printoptions(suppress=True)
 
+
+
+def ll_table(maindir,year,N_TRIAL,trajectories, feature_matrices,discount, Tprob):
+
+    outname='results/ll_table.csv'
+    if os.path.exists(outname):
+        return pd.read_csv(outname)
+
+    df = pd.DataFrame().from_dict(read_multi_data(maindir,year))
+    fnames = df.fname.values
+    ll_list = []
+    for f in fnames:
+        weights = np.load(f)[-1,-1,-1,:] # last updated weights
+        ll_list.append(likelihood(N_TRIAL,trajectories, feature_matrices, weights, discount, Tprob))
+    df['LL'] = pd.Series(ll_list)
+    df.to_csv('results/ll_table.csv')
+
+    return df
+
+
+
 maindir = '/data/neuroventure/behavioral/nback_and_bart/rl_bart'
 year=2
 N=138
@@ -20,25 +41,29 @@ N_TRIAL, N_STATES, N_FEAT = np.shape(feature_matrices[0])
 N_STATES -= 2
 
 gradients = np.load(f'results/gradients_V{year}.npy')
-weights = np.load(f'results/theta_V{year}.npy')
+#weights = np.load(f'results/theta_V{year}.npy')
+weights = np.load('results/theta_V2_N148_E1000_LR0.0001_LRD5.npy')
 
-# pass the average weights over the 30 trajectories, over the N experts
-avg_weights = weights.mean(axis=1).mean(axis=1)[-1]
+# not really the avg, just last update
+avg_weights = weights[-1,-1,-1,:]
+#avg_weights = np.array([ 1.12415715,  0.95328001,  2.06785323,  0.6785101,   0.72629839,  0.57598924,
+ #         0.80497392,  0.38839539,  0.22136966,  1.03345905, -0.04700773])
 
 obs_exp_rewards, avg_save_state = get_stats(N_EXPERTS,N_TRIAL,N_STATES,N_FEAT,trajectories)
 
 ### PLOTTING ###
 
 # multi run results 
-run_data=read_multi_data('results',year,kind="theta")
-plot_multi_weights(run_data)
+#run_data=read_multi_data('results',year,kind="theta")
+#plot_multi_weights(run_data)
 
 #plot_reward_landscape(N_EXPERTS,N_TRIAL,N_STATES,N_FEAT,avg_weights,feature_matrices,obs_exp_rewards,avg_save_state,'line',clobber=True)
 #plot_gradients(gradients)
 #plot_weights(weights,'line')
 
-avg_LL = likelihood(N_TRIAL,trajectories, feature_matrices, avg_weights, discount=1, Tprob=Tprob)
+#avg_LL = likelihood(N_TRIAL,trajectories, feature_matrices, avg_weights, discount=1, Tprob=Tprob)
 
-print(f"Average Log Likelihood on training demonstrations: N = {N_EXPERTS*N_TRIAL} demonstrations, LL = {avg_LL}")
+print(ll_table('results',year,N_TRIAL,trajectories, feature_matrices,1, Tprob))
+#print(f"Average Log Likelihood on training demonstrations: N = {N_EXPERTS*N_TRIAL} demonstrations, LL = {avg_LL}")
 
 
