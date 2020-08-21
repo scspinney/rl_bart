5f6a416d694e6e7d7583a87e8d6f55a7a78a854f
SpinnSean <sean.c.spinney@gmail.com>
2020-08-20 21:01:44 -0400

    Modified compare learner functions.

--- compare_learners.py
+++ compare_learners.py
@@ -10,15 +10,17 @@ maindir = '/data/neuroventure/behavioral/nback_and_bart/rl_bart'
 year=2
 
 feature_matrices, Tprob, trajectories = load_data(maindir,year)
+ai_players = {'human': 'results/theta_V2_N138_E100_LR0.0001_LRD1_S100.npy',
+              'qlearner': 'results/QL_ai_theta_V2_N138_E100_LR0.0001_LRD1_S100.npy',
+              'alwayspump': 'results/AlwaysPump_ai_theta_V2_N138_E100_LR0.0001_LRD1_S100.npy',
+              'random': 'results/Random_ai_theta_V2_N138_E100_LR0.0001_LRD1_S100.npy'}
 
 
 # get weights
-human_weights = process_weights('results/theta_V2_N138_E100_LR0.0001_LRD1_S100.npy','humans')
-ai_weights1 = process_weights('results/QL_ai_theta_V2_N138_E100_LR0.0001_LRD1_S100.npy','qlearner')
-ai_weights2 = process_weights('results/AlwaysPump_ai_theta_V2_N138_E100_LR0.0001_LRD1_S100.npy','alwayspump')
-ai_weights3 = process_weights('results/Random_ai_theta_V2_N138_E100_LR0.0001_LRD1_S100.npy','random')
-ai_weights = ai_weights1.append(ai_weights2.append(ai_weights3))
-all_weights = human_weights.append(ai_weights)
+all_weights = process_weights(ai_players['human'],'human')
+
+for p,f in ai_players.items():
+    all_weights.append(process_weights(f,p))
 
 # bar plots of the weights
 plot_weights_bar(all_weights)
@@ -31,8 +33,8 @@ N_FEAT = 11
 
 obs_exp_rewards, avg_save_state = get_stats(N_EXPERTS,N_TRIAL,N_STATES,N_FEAT,trajectories)
 
-for player_type in all_weights.Player.unique():
-    data = all_weights.query(f"Player == {player_type}")
-    data=data.sort_values(by="Features")
-    weights = data['Weights'].values
-    plot_reward_landscape(N_EXPERTS,N_TRIAL,N_STATES,N_FEAT,weights,feature_matrices,obs_exp_rewards,avg_save_state)
+for p,f in ai_players.items():
+    weights = np.load(f)
+    weights = weights[-1].mean(axis=(0,1))
+
+    plot_reward_landscape(p,N_EXPERTS,N_TRIAL,N_STATES,N_FEAT,weights,feature_matrices,obs_exp_rewards,avg_save_state)
